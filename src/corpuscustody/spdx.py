22e7eef1595db52c6da0ac2d9c20819714abec22
frankyboyzeln <23084899+frankyboyzeln@users.noreply.github.com>
2021-12-13 17:00:00 +0700

    fix: correct roadmap milestones

---
+++ src/corpuscustody/spdx.py
@@ -0,0 +1,10 @@
+"""Offline SPDX identifier table with mechanical obligations.
+
+This is a small, hand-maintained table of common SPDX identifiers mapped to the
+obligations that matter for combining datasets. It is a mechanical lookup, not a
+legal interpretation. See the README limitations section.
+
+Obligations tracked per license:
+  attribution     the license requires preserving attribution or notices
+  share_alike     derivatives or the combined work must carry the same license
+  non_commercial  commercial use is not permitted
