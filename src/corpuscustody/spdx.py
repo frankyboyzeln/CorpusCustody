07ec2d0a201cad602064195ba380ff861ce0a9d6
frankyboyzeln <23084899+frankyboyzeln@users.noreply.github.com>
2021-12-16 18:27:00 +0700

    fix: correct unknown license rule

--- src/corpuscustody/spdx.py
+++ src/corpuscustody/spdx.py
@@ -8,3 +8,11 @@ Obligations tracked per license:
   attribution     the license requires preserving attribution or notices
   share_alike     derivatives or the combined work must carry the same license
   non_commercial  commercial use is not permitted
+  no_derivatives  modified or derived works are not permitted
+  unknown         provenance or license is not established
+
+The `unknown` obligation is never inferred as permissive. A record with no
+resolvable license is treated as carrying every restrictive obligation, so it
+can never silently pass a gate.
+"""
+
