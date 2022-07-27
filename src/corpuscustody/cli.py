f3e9b961c2a05c6caf5f890dc0f745f710be9002
frankyboyzeln <23084899+frankyboyzeln@users.noreply.github.com>
2022-07-27 09:07:00 +0700

    style: clean up pairwise compatibility

--- src/corpuscustody/cli.py
+++ src/corpuscustody/cli.py
@@ -10,3 +10,15 @@ Exit codes:
   0  clean, gate passed, or informational command succeeded
   1  gate refused (findings present)
   2  usage error or bad input
+"""
+
+import argparse
+import sys
+from typing import List, Optional
+
+from . import __version__
+from .compat import PurposeError, PURPOSES
+from .gate import decide, write_cleared_manifest
+from .manifest import ManifestError, parse_file
+from .report import render_gate, render_report, render_resolve
+
